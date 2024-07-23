e0de89f8661f06f7a8f9e3aa73745a831ba308b7
allbzzz <146387808+allbzzz@users.noreply.github.com>
2024-07-23 12:53:38 +0300

    Add files via upload

---
+++ lab_13_z1_v2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static double Calculate(double x, int n)
+        {
+            return Math.Pow(x, n) / n;
+        }
+        static void Main(string[] args)
+        {
+            double x = 2;
+            double result = Calculate(x, 2) + Calculate(x, 4) + Calculate(x, 6);
+            Console.WriteLine(result);
+        }
+    }
+}
