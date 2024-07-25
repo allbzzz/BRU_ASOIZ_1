a91db7e1709d8ec96111e25b97098055df15005c
allbzzz <146387808+allbzzz@users.noreply.github.com>
2024-07-25 13:18:47 +0300

    Add files via upload

---
+++ lab_14_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "powers_of_3.txt";
+            int n = 10; // Количество степеней числа 3
+
+            // Создание и запись
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int power = (int)Math.Pow(3, i);
+                    writer.WriteLine(power);
+                }
+            }
+
+            // Вывод 
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                int lineNumber = 1;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (lineNumber % 2 == 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    lineNumber++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
