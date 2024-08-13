6014688656c00aa2d538939b74ceecdd72a37077
allbzzz <146387808+allbzzz@users.noreply.github.com>
2024-08-13 14:14:42 +0300

    Add files via upload

---
+++ lab1_z2_v4.cs
@@ -0,0 +1,37 @@
+/*
+Лабораторная 1, задание 2, вариант 4
+Даны две переменные целого типа: A и B. Если их значения не равны, 
+то присвоить каждой переменной сумму этих значений, а если равны, то 
+присвоить переменным нулевые значения. Вывести новые значения 
+переменных A и B.
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"До обработки: A = {A}, B = {B}");
+
+        if (A != B)
+        {
+            int sum = A + B;
+            A = sum;
+            B = sum;
+        }
+        else
+        {
+            A = 0;
+            B = 0;
+        }
+
+        Console.WriteLine($"После обработки: A = {A}, B = {B}");
+    }
+}
