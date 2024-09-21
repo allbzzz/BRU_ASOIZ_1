ae98b34924adaff278cc634875d49c58904a095d
allbzzz <146387808+allbzzz@users.noreply.github.com>
2024-09-21 11:26:25 +0300

    Add files via upload

--- lab3_z2_v1.cs
+++ lab3_z2_v1.cs
@@ -1,44 +1,29 @@
 /*
-Лабораторная работа 3
-Задание 2
-1
-Ввести с клавиатуры длины катетов a и b прямоугольного треугольника. 
-Найти его периметр и площадь.
+Лабораторная 3, задание 2, вариант 1
+Найти периметр и площадь равнобедренной трапеции с основаниями a и b
+(a > b) и углом alpha при большем основании (угол дан в радианах).
 
 */
 
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp2
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            try
-            {
-                int a, b;
-                double p, s;
-                Console.WriteLine("Введите длину первого катета: ");
-                a = int.Parse(Console.ReadLine());
-                Console.WriteLine("Введите длину второго катета: ");
-                b = int.Parse(Console.ReadLine());
-                double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-                p = a + b + c;
-                s = (a * b) / 2.0;
-                Console.WriteLine($"Периметр треугольника с катетами {a} и {b} = {p:f2}\nПлощадь треугольника с катетами {a} и {b} = {s:f2}");
-            }
-            catch(Exception e)
-            {
-                string message = e.Message;
-                Console.WriteLine("Ошибка: {0}", message);
-            }
-            Console.ReadKey();
-        }
+    static void Main() {
+        Console.Write("Введите длину первого катета a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите длину второго катета b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double c = Math.Sqrt(a * a + b * b);
+
+        double perimeter = a + b + c;
+
+        double area = (a * b) / 2;
+
+        Console.WriteLine($"Периметр треугольника: {perimeter:F2}");
+        Console.WriteLine($"Площадь треугольника: {area:F2}");
     }
 }
