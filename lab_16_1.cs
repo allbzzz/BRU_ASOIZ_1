1e46f21b696a1d37125b6dd39930f32f6ec7b98c
allbzzz <146387808+allbzzz@users.noreply.github.com>
2025-02-02 22:02:25 +0300

    Add files via upload

---
+++ lab_16_1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "powers_of_3.bin";
+            int n = 10; // Количество степеней числа 3
+
+            // Создание и запись
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int power = (int)Math.Pow(3, i);
+                    writer.Write(power);
+                }
+            }
+
+            // Вывод 
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                int recordNumber = 1;
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    int number = reader.ReadInt32();
+                    if (recordNumber % 2 == 0)
+                    {
+                        Console.WriteLine(number);
+                    }
+                    recordNumber++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
