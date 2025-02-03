0713af2cc83cdeb87afde8f7f8dc5f24573da766
allbzzz <146387808+allbzzz@users.noreply.github.com>
2025-02-03 19:49:56 +0300

    Add files via upload

---
+++ lab4_z2_v8_2.cs
@@ -0,0 +1,33 @@
+/*
+Лабораторная 4, задание 2, вариант 8
+Даны три числа. Вывести вначале меньшее, а затем большее из данных 
+чисел.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+
+        Console.Write("Введите первое число: ");
+        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+
+        Console.Write("Введите второе число: ");
+        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+
+        Console.Write("Введите третье число: ");
+        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+
+        int min = numbers.Min();
+        int max = numbers.Max();
+
+        Console.WriteLine($"Наименьшее число: {min}");
+        Console.WriteLine($"Наибольшее число: {max}");
+    }
+}
